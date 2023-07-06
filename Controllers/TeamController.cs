d3b57b82481b3e5c52844d601644c45493bbf4ee
Gicu <gicurata@gmail.com>
2023-07-06 18:02:43 +0300

    Team details functionality, not full

--- Controllers/TeamController.cs
+++ Controllers/TeamController.cs
@@ -13,20 +13,77 @@ namespace CompetitionManagment.Controllers
         {
             _context = context;
         }
+
         public IActionResult Index()
         {
             return View(_context.Teams.ToList());
         }
+
         public IActionResult Create()
         {
             return View();
         }
+
         [HttpPost]
         public IActionResult Create(Team team)
         {
-            _context.Teams.Add(team);
+            if (team.CreatedOn > DateTime.Today)
+            {
+                ModelState.AddModelError("CreatedOn", "The date cannot be in the future.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Teams.Add(team);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(team);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var team = _context.Teams.Find(id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+            return View(team);
+        }
+        [HttpPost]
+        public IActionResult Edit(Team team)
+        {
+            if (team.CreatedOn > DateTime.Today)
+            {
+                ModelState.AddModelError("CreatedOn", "The date cannot be in the future.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Entry(team).State = EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(team);
+        }
+
+
+
+        public IActionResult Delete(int id)
+        {
+            var team = _context.Teams.Find(id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+            _context.Teams.Remove(team);
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        
+
+
     }
+
 }
